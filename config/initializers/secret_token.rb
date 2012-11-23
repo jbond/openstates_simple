707c3e43d87f27f2fe36756eae20bdc49ea31c89
James Bond <jamesbond.home@gmail.com>
2012-11-22 22:06:09 -0800

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenstatesSimple::Application.config.secret_token = 'e7d4a63563faa97fde25baa2f11ed3690e9ebfbcd11397e01c7b2864ce3101f0cc734a4c5cabff0ca5360b96bbfb1c4771b5adf26d0f495d97cf9a735d788858'
